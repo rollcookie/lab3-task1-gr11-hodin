812a12b73c1ee415de7253f824bb6de858d0c3ac
rollcookie <154047723+rollcookie@users.noreply.github.com>
2024-03-06 16:03:31 +0000

    update main.c

--- .ccls-cache/@home@runner@lab3-task1-gr11-hodin/src@main.c
+++ .ccls-cache/@home@runner@lab3-task1-gr11-hodin/src@main.c
@@ -1,8 +1,37 @@
 /* hello.c */
 #include <stdio.h>
+#include <stdbool.h>
+
+bool is_happy(int n)
+{
+  if (n > 999999 || n < 100000)
+  {
+    return false;
+  }
+  int sum = 0;
+  while (n > 1000)
+  {
+    sum += n % 10;
+    n /= 10;
+  }
+
+  while (n > 0)
+  {
+    sum -= n % 10;
+    n /= 10;
+  }
+
+  return sum == 0;
+}
 
 void main (void)
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+  for(int i = 100000; i < 1000000; i++)
+  {
+    if(is_happy(i))
+    {
+      printf("%d\n", i);
+    }
+  }
+
 }
